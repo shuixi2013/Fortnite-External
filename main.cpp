bdbb1b3f9b69fee370c728318f1abe518852f2d3
Chase <70964202+Chase1803@users.noreply.github.com>
2022-04-16 04:29:48 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
+/*
+	Copyright (c) 2022 Chase1803
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * Gloomy.cc
+ * Made by Chase
+ * https://github.com/Chase1803
+*/
+
 #include <iostream>
 #include "GL/glew.h"
 #include "imgui/imgui.h"
@@ -1821,8 +1849,7 @@ int main() {
 	}
 
 	g_base_address = getBaseAddress(g_pid);
-
-	//printf(XorStr("[#] Fortnite.exe\r").c_str());
+	
 	if (!pattern_uworld)
 		pattern_uworld = find_signature(XorStr("\x48\x89\x05\x00\x00\x00\x00\x48\x8B\x4B\x78").c_str(), XorStr("xxx????xxxx").c_str());
 
@@ -1852,10 +1879,8 @@ int main() {
 
 	while (!glfwWindowShouldClose(g_window))
 	{
-		//if (isFortniteFocused) {
 		handleKeyPresses();
 		runRenderTick();
-		//	}
 	}
 
 	cleanupWindow();
