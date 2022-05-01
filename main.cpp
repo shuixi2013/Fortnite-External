1e55f8b1d6ddecc30a087daae2fa7e852d4f9bfc
Chase <70964202+Chase1803@users.noreply.github.com>
2022-05-01 16:41:50 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1213,6 +1213,25 @@ void runRenderTick() {
 	glfwSwapBuffers(g_window);
 }
 
+enum InjectedInputMouseOptions
+{
+    Absolute = 32768,
+    HWheel = 4096,
+    LeftDown = 2,
+    LeftUp = 4,
+    MiddleDown = 32,
+    MiddleUp = 64,
+    Move = 1,
+    MoveNoCoalesce = 8192,
+    None = 0,
+    RightDown = 8,
+    RightUp = 16,
+    VirtualDesk = 16384,
+    Wheel = 2048,
+    XDown = 128,
+    XUp = 256
+};
+
 typedef struct _InjectedInputMouseInfo
 {
 	int DeltaX;
