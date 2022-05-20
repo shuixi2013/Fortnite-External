4b1385f0592ae1fa2ae28c84cbd13c444d9c35c7
Chase <70964202+Chase1803@users.noreply.github.com>
2022-05-20 20:30:43 +0530

    Update decryption.asm

--- decryption.asm
+++ decryption.asm
@@ -69,7 +69,7 @@ static std::string ReadGetNameFromFName(int key) {
 	uint32_t ChunkOffset = (uint32_t)((int)(key) >> 16);
 	uint16_t NameOffset = (uint16_t)key;
  
-	uint64_t NamePoolChunk = read<uint64_t>(g_pid, g_base_address + 0xC6F3C00 + (8 * ChunkOffset) + 16) + (unsigned int)(4 * NameOffset); // ERROR_NAME_SIZE_EXCEEDED
+	uint64_t NamePoolChunk = read<uint64_t>(g_pid, pattern_gnames + (8 * ChunkOffset) + 16) + (unsigned int)(4 * NameOffset); // ERROR_NAME_SIZE_EXCEEDED
 	uint16_t nameEntry = read<uint16_t>(g_pid, NamePoolChunk);
  
 	int nameLength = nameEntry >> 6; 
@@ -114,7 +114,7 @@ static std::string GetNameFromFName(int key)
 	uint32_t ChunkOffset = (uint32_t)((int)(key) >> 16);
 	uint16_t NameOffset = (uint16_t)key;
  
-	uint64_t NamePoolChunk = read<uint64_t>(g_pid, g_base_address + 0xC6F3C00 + (8 * ChunkOffset) + 16) + (unsigned int)(4 * NameOffset); // ERROR_NAME_SIZE_EXCEEDED
+	uint64_t NamePoolChunk = read<uint64_t>(g_pid, pattern_gnames + (8 * ChunkOffset) + 16) + (unsigned int)(4 * NameOffset); // ERROR_NAME_SIZE_EXCEEDED
 	if (read<uint16_t>(g_pid, NamePoolChunk) < 64)
 	{
 		auto a1 = read<DWORD>(g_pid, NamePoolChunk + 4);
