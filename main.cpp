bc8f4aadd81866e1e2ccbd57eeb2213609b8b77d
Chase <70964202+Chase1803@users.noreply.github.com>
2022-05-22 13:22:49 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1313,11 +1313,8 @@ int main() {
 
 	g_base_address = getBaseAddress(g_pid);
 
-	if (!pattern_uworld)
-		pattern_uworld = find_signature(XorStr("\x48\x89\x05\x00\x00\x00\x00\x48\x8B\x4B\x78").c_str(), XorStr("xxx????xxxx").c_str());
-
-	//if (!pattern_gnames)
-//	pattern_gnames = find_signature(XorStr("\x48\x83\xEC\x28\x80\x3D\x9D\x12\x82\x0B\x00\x74\x0C\x48\x8D\x05\x98").c_str(), XorStr("xxxxxxxxxx?xxxxxx").c_str());
+	if (!pattern_uworld) pattern_uworld = find_signature(XorStr("\x48\x89\x05\x00\x00\x00\x00\x48\x8B\x4B\x78").c_str(), XorStr("xxx????xxxx").c_str());
+	if (!pattern_gnames) pattern_gnames = PatternScan<uint64_t>("48 83 EC 28 80 3D 9D 12 82 0B 00 74 0C 48 8D 05 98", 10);
 
 	if (!g_base_address) {
 		std::cout << XorStr("Could not get base address.\n").c_str();
